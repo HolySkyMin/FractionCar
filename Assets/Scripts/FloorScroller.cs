c9c06121b8c859ed29f2efa13b84e001cb46ce5c
NoMansLand <fsda109238@gmail.com>
2019-08-03 14:14:49 +0900

    클릭킹 메커니즘 구현

---
+++ Assets/Scripts/FloorScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScroller : MonoBehaviour
+{
+    public GameObject[] FloorObject;
+    public float Distance;
+
+    private Queue<GameObject> floorQueue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorQueue = new Queue<GameObject>();
+        for (int i = 0; i < FloorObject.Length; i++)
+            floorQueue.Enqueue(FloorObject[i]);
+    }
+
+    public void MoveFloor()
+    {
+        foreach (var obj in floorQueue)
+        {
+            obj.transform.Translate(0, -2, 0);
+        }
+
+        if (floorQueue.Peek().transform.localPosition.y <= Distance * -1)
+        {
+            floorQueue.Peek().transform.localPosition = new Vector3(0, Distance, 0);
+            floorQueue.Enqueue(floorQueue.Dequeue());
+        }
+    }
+}
