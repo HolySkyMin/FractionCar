8e274edef9b6f555f83267f4d38019aea42c2899
@recker333 <backsmash1234@gmail.com>
2019-08-10 13:19:18 +0900

    ddd

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,8 +9,12 @@ public class Character : MonoBehaviour
 {
     
     public bool CurrentLine;
+    public bool CurrentLine2;
     public DataController dataController;
     public FloorScroller Scroller;
+    Vector3 vector;
+    Vector3 vector2;
+
    
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,8 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        vector.x = -30;
+        vector2.x = 30;
         CurrentLine = true;
         // TODO: 오브젝트 움직이는 기능 : 양엎으로까지.
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
@@ -37,20 +43,41 @@ public class Character : MonoBehaviour
             Scroller.MoveFloor();
             
         }
+        if (vector.x == this.transform.position.x)
+        {
+            CurrentLine = false;
+        }
+        else
+        {
+            CurrentLine = true;
+        }
+        if (vector2.x == this.transform.position.x)
+        {
+            CurrentLine2 = false;
+        }
+        else
+        {
+            CurrentLine2 = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (CurrentLine == true)
             {
-                this.transform.Translate(-22, 0, 0);
+                this.transform.Translate(-30, 0, 0);
+                
             }
-            
-            
+           
+
+
+
+
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (CurrentLine == true)
+            if (CurrentLine2 == true)
             {
-                this.transform.Translate(22, 0, 0);
+                this.transform.Translate(30, 0, 0);
             }
             
         }
