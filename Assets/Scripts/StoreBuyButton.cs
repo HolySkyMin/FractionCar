236e7fb12103ca9c741dc8c5e6275a3a5ade404d
jaemin726 <hoyden80@naver.com>
2019-08-17 15:15:49 +0900

    상점 화면 보강

--- Assets/Scripts/StoreBuyButton.cs
+++ Assets/Scripts/StoreBuyButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreBuyButton : MonoBehaviour
 {
@@ -9,13 +10,22 @@ public class StoreBuyButton : MonoBehaviour
         switch(index)
         {
             case 0:
-                
+                if (IngameManager.Instance.Data.CurMoney < 100)
+                    break;
+                IngameManager.Instance.Data.CurMoney -= 100;
+                IngameManager.Instance.Data.AbilityValue[0] += 1;
                 break;
             case 1:
-
+                if (IngameManager.Instance.Data.CurMoney < 100)
+                    break;
+                IngameManager.Instance.Data.CurMoney -= 100;
+                IngameManager.Instance.Data.AbilityValue[1] += 1;
                 break;
             case 2:
-
+                if (IngameManager.Instance.Data.CurMoney < 100)
+                    break;
+                IngameManager.Instance.Data.CurMoney -= 100;
+                IngameManager.Instance.Data.AbilityValue[2] += 1;
                 break;
         }
     }
